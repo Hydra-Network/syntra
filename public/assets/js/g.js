64239125d316abcdb5eb229922d6a456dd2fa87c
rogo! <r480github@gmail.com>
2026-03-28 23:42:09 -0400

    add pin

--- public/assets/js/g.js
+++ public/assets/js/g.js
@@ -1,23 +1,34 @@
 const gme_list = document.querySelector(".allGcontainer");
+const pinned_list = document.querySelector(".pinnedGcontainer");
 const gmes_search = document.querySelector("input[type='text']");
-
+let pinnedG = JSON.parse(localStorage.getItem("pinnedGLocal")) || [];
+console.log(pinnedG);
 let gmes_data = [];
 
 const render_gmes = (games) => {
   gme_list.innerHTML = "";
+  pinned_list.innerHTML = "";
   games.forEach((game) => {
     const card = document.createElement("div");
+    const pinned_list = document.querySelector(".pinnedGcontainer");
     card.className = "g";
+    if (pinnedG.includes(game.file_name)) {
+      card.classList.add("pinned");
+      pinned_list.appendChild(card);
+    } else {
+      gme_list.appendChild(card);
+      
+    }
+
     card.innerHTML = `
-<h3 class="gTitle">${game.title}</h3>
-						<img class="gImg" src='https://raw.githubusercontent.com/Hydra-Network/hydra-assets/main/${game.thumb}'
+    <img id="pin" class="pin" src="/assets/img/pin.png" onclick="togglePin('${game.file_name}')" loading="lazy">
+    <h3 class="gTitle">${game.title}</h3>
+		<img onclick="open_gme('${game.file_name}')" id="gImg" class="gImg" src='https://raw.githubusercontent.com/Hydra-Network/hydra-assets/main/${game.thumb}' loading="lazy"
 		alt = "" > `;
-    card.addEventListener("click", () => open_gme(game.file_name));
-    gme_list.appendChild(card);
   });
 };
 const frame = document.getElementById("gFrame");
-
+const pin = document.getElementById("pin");
 const open_gme = async (file_name) => {
   localStorage.setItem("gName", file_name);
   frame.src = "/g/gSrc.html";
@@ -25,6 +36,7 @@ const open_gme = async (file_name) => {
   frame.style.opacity = "1";
   const goBackBtn = document.getElementById("goBackBtn");
   goBackBtn.style.top = "20px";
+  document.body.style.overflow = "hidden";
 };
 goBackBtn.addEventListener("click", () => {
   frame.style.zIndex = "-10";
@@ -32,6 +44,7 @@ goBackBtn.addEventListener("click", () => {
   document.documentElement.style.overflow = "";
   goBackBtn.style.top = "-80px";
   frame.style.opacity = "0";
+  document.body.style.overflow = "visible";
 });
 
 gmes_search.addEventListener("input", (e) => {
@@ -41,6 +54,17 @@ gmes_search.addEventListener("input", (e) => {
   );
   render_gmes(filtered);
 });
+
+function togglePin(f) {
+  if (pinnedG.includes(f)) {
+    pinnedG = pinnedG.filter((g) => g !== f);
+    1;
+  } else {
+    pinnedG.push(f);
+  }
+  localStorage.setItem("pinnedGLocal", JSON.stringify(pinnedG));
+  render_gmes(gmes_data);
+}
 
 (async () => {
   try {
